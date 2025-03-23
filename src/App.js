08eaeda2162d9ad67bd5cd13dec151b323b7c1a3
G-Kesavan <mr.kesavaperumal@gmail.com>
2025-03-23 23:50:55 +0530

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import HomeScreen from "./Pages/HomeScreen/HomeScreen";
+import './App.css';
 
 const App = () => {
+  const user={
+    name:"kesavan",
+    email:"kesavan@gmail.com"
+  };
   return (
-    <div>
-      hello
-    </div>
-  )
-}
+    <div className='app'>
+      <Router>
+        {!user? (
+          
+          <h1>login screen</h1>
 
-export default App
+        ) : (
+          <Routes>
+            {/* <Route path='/profile' element={}/> */}
+            <Route path='/' element={<HomeScreen />}/>
+          </Routes>
+          
+        )}
+      </Router>
+    </div>
+  );
+};
+
+export default App;

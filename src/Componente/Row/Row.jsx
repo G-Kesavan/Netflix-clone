0f16fc8f23686108cdb373134287ba3993a4d13c
GOOD WELL <mr.kesavaperumal@gmail.com>
2025-07-04 21:11:35 +0530

    ...

---
+++ src/Componente/Row/Row.jsx
@@ -0,0 +1,43 @@
+import axios from '../../API/axios'
+import React, { useEffect,useState } from 'react'
+import './Row.css'
+
+
+const Row = ({title,fetchURL,isLarge}) => {
+
+const baseURL = "https://image.tmdb.org/t/p/original/";
+const [movie, setMovie] = useState([])
+
+useEffect(() => {
+    const fetchData =async()=>{
+        const request= await axios.get(fetchURL);
+        setMovie(request.data.results);
+        return request;
+    }
+    fetchData()   
+}, [fetchURL])
+
+console.log(movie)
+  return (
+    <div>
+        <h2>{title}</h2>
+        <div className="row-posters">
+            {movie.map((movie)=>
+              (isLarge && movie.poster_path)||
+              (!isLarge && movie.backdrop_path)&&
+              (<img 
+                    key={movie.id} 
+
+                    className={`row-poster ${isLarge && 'row__posterLarge'}`}
+
+                    src={`${baseURL}${isLarge ? movie.poster_path: movie.backdrop_path}`}
+
+                    alt='photo'
+                />)
+            )}
+        </div>
+    </div>
+  )
+}
+
+export default Row
--- src/Pages/Home/HomeScreen.jsx
+++ src/Pages/Home/HomeScreen.jsx
@@ -2,12 +2,52 @@ import React from 'react'
 import './HomeScreen.css'
 import Navber from '../../Componente/Navbar/Navbar'
 import Banner from '../../Componente/Banner/Banner'
+import Row from '../../Componente/Row/Row'
+import Request from '../../API/request'
+import axios from '../../API/axios'
 
 const HomeScreen = () => {
   return (
     <div>
       <Navber/>
       <Banner/>
+      <Row
+        title={"NETFLIX ORIGINALS"}
+        fetchURL={Request.fetchNetflixOriginals}
+        isLarge={true}
+      />
+      <Row
+        title={"Trending Now"}
+        fetchURL={Request.fetchTrending}
+        isLarge={false}
+      />
+      <Row
+        title={"Comady"}
+        fetchURL={Request.fetchComedyMovies}
+        isLarge={false}
+      />
+      <Row
+        title={"Horror"}
+        fetchURL={Request.fetchHorrorMovies}
+        isLarge={false}
+      />
+      <Row
+        title={"Action"}
+        fetchURL={Request.fetchActionMovies}
+        isLarge={false}
+      />
+
+      <Row
+        title={"Romance"}
+        fetchURL={Request.fetchRomanceMovies}
+        isLarge={false}
+      />
+
+      <Row
+        title={"Documentaries"}
+        fetchURL={Request.fetchDocumentaries}
+        isLarge={false}
+      />
     </div>
   )
 }
